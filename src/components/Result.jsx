c4ce972ed0a3e6c6e7983c24017c85c7ca980573
“minnji-kim” <ohmygmg@naver.com>
2024-07-05 11:04:18 +0900

    name

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,11 +1,11 @@
-import ESTimg from '../image/포메라니안.jpeg';
-import ESFimg from '../image/푸들.jpeg';
-import ENTimg from '../image/스피츠.jpeg';
-import ENFimg from '../image/요크셔테리어.jpeg';
-import ISTimg from '../image/보더콜리.jpeg';
-import ISFimg from '../image/골든리트리버.jpeg';
-import INTimg from '../image/차우차우.jpeg';
-import INFimg from '../image/시바견.JPG';
+import ESTimg from '../image/ESTimg.jpeg';
+import ESFimg from '../image/ESFimg.jpeg';
+import ENTimg from '../image/ENTimg.jpeg';
+import ENFimg from '../image/ENFimg.jpeg';
+import ISTimg from '../image/ISTimg.jpeg';
+import ISFimg from '../image/ISFimg.jpeg';
+import INTimg from '../image/INTimg.jpeg';
+import INFimg from '../image/INFimg.JPG';
 import { useNavigate } from 'react-router';
 
 const results = {
